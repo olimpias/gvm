f5bec6e36f0c4d6ae478891e13342a41413287de
Cem Türker <cemturker92@gmail.com>
2020-07-05 21:41:46 +0200

    Print version in CLI (#11)
    * Print version in CLI

---
+++ version/version.go
@@ -0,0 +1,18 @@
+package version
+
+import (
+	"fmt"
+
+	"github.com/olimpias/gvm/logger"
+)
+
+const (
+	major = 0
+	minor = 0
+	patch = 1
+)
+
+func Print() {
+	version := fmt.Sprintf("%d.%d.%d", major, minor, patch)
+	logger.Info(fmt.Sprintf("gvm version %s \n", version))
+}
